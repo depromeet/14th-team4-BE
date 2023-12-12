42c5dabbccebe7fc967260e68616c7f608efad28
Ryuhyis <soo990315@naver.com>
2023-12-12 22:33:36 +0900

    [feat] jwt test용 api 추가

--- server-api/src/main/java/com/depromeet/controller/HomeController.java
+++ server-api/src/main/java/com/depromeet/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.depromeet.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.depromeet.user.entity.User;
 
 @RestController
 public class HomeController {
@@ -16,5 +19,11 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home() {
 		return "hello home";
+	}
+
+	// test
+	@GetMapping("/jwt-test")
+	public User getUserInfo(@AuthenticationPrincipal User user) {
+		return user;
 	}
 }
