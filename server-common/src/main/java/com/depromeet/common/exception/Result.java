9667d7a0bf656a27f25e89741d209d1a8792b592
유희수 <soo990315@naver.com>
2024-01-11 17:56:33 +0900

    [feat] response, error 타입 추가

--- server-common/src/main/java/com/depromeet/common/exception/Result.java
+++ server-common/src/main/java/com/depromeet/common/exception/Result.java
@@ -6,13 +6,15 @@ import lombok.Getter;
 public enum Result {
 
     OK(200, "성공"),
+    CREATED(201, "생성"),
     LOGOUT_OK(200, "로그아웃 성공"),
     DELETE_OK(200, "회원 탈퇴 성공"),
     FAIL(400, "실패"),
     BAD_REQUEST(400,"잘못된 요청"),
     UNAUTHORIZED_USER(403, "권한 없는 사용자"),
     NOT_FOUND_BOOKMARK(404, "북마크를 찾을 수 없습니다."),
-    NOT_FOUND_STORE(404, "가게를 찾을 수 없습니다."),;
+    NOT_FOUND_STORE(404, "가게를 찾을 수 없습니다."),
+    NOT_FOUND_CATEGORY(404, "카테고리를 찾을 수 없습니다." );
 
 
 
