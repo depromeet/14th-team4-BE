2db96ce6cfd44adeeabe5c09a9c8afc7d9624d11
highjune <highjune37@gmail.com>
2024-01-11 19:15:55 +0900

    [domain] StoreReviewResponse 추가

--- server-api/src/main/java/com/depromeet/domains/store/dto/response/StoreReviewResponse.java
+++ server-api/src/main/java/com/depromeet/domains/store/dto/response/StoreReviewResponse.java
@@ -1,32 +1,33 @@
 package com.depromeet.domains.store.dto.response;
 
+import java.time.LocalDateTime;
+
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class StoreReviewResponse {
 
-    private Long userId;
-    private String nickName;
-    private Float rating;
-    private String imageUrl;
-    private Integer visitTimes;
-    private LocalDateTime visitedAt;
-    private String description;
+	private Long userId;
+	private String nickName;
+	private Float rating;
+	private String imageUrl;
+	private Integer visitTimes;
+	private LocalDateTime visitedAt;
+	private String description;
 
-    public static StoreReviewResponse of(Long userId, String nickName, Float rating, String imageUrl, Integer visitTimes, LocalDateTime visitedAt, String description) {
-        return StoreReviewResponse.builder()
-                .userId(userId)
-                .nickName(nickName)
-                .rating(rating)
-                .imageUrl(imageUrl)
-                .visitTimes(visitTimes)
-                .visitedAt(visitedAt)
-                .description(description)
-                .build();
-    }
+	public static StoreReviewResponse of(Long userId, String nickName, Float rating, String imageUrl,
+		Integer visitTimes, LocalDateTime visitedAt, String description) {
+		return StoreReviewResponse.builder()
+			.userId(userId)
+			.nickName(nickName)
+			.rating(rating)
+			.imageUrl(imageUrl)
+			.visitTimes(visitTimes)
+			.visitedAt(visitedAt)
+			.description(description)
+			.build();
+	}
 
 }
