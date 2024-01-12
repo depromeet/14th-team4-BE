bfa99c101f7cbc108aaf64db74150c2696dd58d0
thisishwan2 <feel000617@gmail.com>
2024-01-12 23:31:01 +0900

    feat: 음식점,카페 검색 서비스

--- server-api/src/main/java/com/depromeet/domains/store/service/StoreService.java
+++ server-api/src/main/java/com/depromeet/domains/store/service/StoreService.java
@@ -57,7 +57,7 @@ public class StoreService {
 			store.getStoreId(),
 			store.getCategory().getCategoryName(),
 			store.getStoreName(),
-			store.getStoreAddress().getRoadAddress(),
+			store.getStoreAddress(),
 			store.getTotalRating(),
 			store.getTotalReviewCount(),
 			logImageUrls,
