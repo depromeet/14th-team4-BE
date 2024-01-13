be5ff57cc49d0fd71a463074c926ffb9a92150fb
thisishwan2 <feel000617@gmail.com>
2024-01-13 18:38:34 +0900

    fix: cors 설정 수정

--- server-api/src/main/java/com/depromeet/config/WebConfig.java
+++ server-api/src/main/java/com/depromeet/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
