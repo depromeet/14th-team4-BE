911c54875e3eaa7cff94c1ef2bd4df51922b1bc3
thisishwan2 <feel000617@gmail.com>
2024-01-13 18:45:43 +0900

    fix: cors 모든 origin open

--- server-api/src/main/java/com/depromeet/config/WebConfig.java
+++ server-api/src/main/java/com/depromeet/config/WebConfig.java
@@ -10,10 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedOrigins("*");
     }
 }
