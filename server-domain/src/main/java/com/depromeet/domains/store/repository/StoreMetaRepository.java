d8210b2cc6eebd601f38dd1cbc7283efe81cffb3
thisishwan2 <feel000617@gmail.com>
2024-01-15 16:10:04 +0900

    feat: StoreMetaRepository 생성

---
+++ server-domain/src/main/java/com/depromeet/domains/store/repository/StoreMetaRepository.java
@@ -0,0 +1,7 @@
+package com.depromeet.domains.store.repository;
+
+import com.depromeet.domains.store.entity.StoreMeta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreMetaRepository extends JpaRepository<StoreMeta, Long> {
+}
