86f801cc4e8ca81d1aa1f4aee16e9b3497bb5b7b
유희수 <soo990315@naver.com>
2024-01-18 02:21:34 +0900

    [test] 로그 추가

--- server-api/src/main/java/com/depromeet/auth/oauth2/handler/CustomAuthenticationRequestFilter.java
+++ server-api/src/main/java/com/depromeet/auth/oauth2/handler/CustomAuthenticationRequestFilter.java
@@ -8,7 +8,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CustomAuthenticationRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -16,13 +18,17 @@ public class CustomAuthenticationRequestFilter extends OncePerRequestFilter {
 
 		if (request.getRequestURI().startsWith("/oauth2/authorization")) {
 			String queryString = request.getQueryString();
+			log.debug("queryString = "+ queryString);
 			if (queryString != null) {
 				String[] keyValuePair = queryString.split("=");
 				String requestEnv = "dev";
 				if (keyValuePair[0].equals("env") && keyValuePair.length > 1) {
 					requestEnv = keyValuePair[1];
 				}
+				log.debug("requestEnv = "+ requestEnv);
 				request.getSession().setAttribute("request_env", requestEnv);
+			} else {
+				request.getSession().setAttribute("request_env", "dev");
 			}
 		}
 		filterChain.doFilter(request, response);
