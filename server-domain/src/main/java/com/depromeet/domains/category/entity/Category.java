e83896202cb2eef3f8259342c0c705eb4b24a8ec
highjune <highjune37@gmail.com>
2024-01-20 00:08:11 +0900

    [feat] feature/#48 Category 테이블에 CategoryType enum 추가

--- server-domain/src/main/java/com/depromeet/domains/category/entity/Category.java
+++ server-domain/src/main/java/com/depromeet/domains/category/entity/Category.java
@@ -1,9 +1,12 @@
 package com.depromeet.domains.category.entity;
 
 import com.depromeet.domains.common.entity.BaseTimeEntity;
+import com.depromeet.enums.CategoryType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,4 +25,8 @@ public class Category extends BaseTimeEntity {
 
 	@Column(nullable = false)
 	private String categoryName;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private CategoryType categoryType;
 }
