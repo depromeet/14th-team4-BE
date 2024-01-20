60f903ba2745ac51d428c13be11c85ebb9bbf70b
유희수 <soo990315@naver.com>
2024-01-20 14:15:43 +0900

    [feat] 401, 403 에러 필터 추가

---
+++ server-api/src/main/java/com/depromeet/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package com.depromeet.config;
+
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.depromeet.common.exception.CustomResponseEntity;
+import com.depromeet.common.exception.Result;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	private final ObjectMapper objectMapper;
+
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException, ServletException {
+		Object errorObj = request.getAttribute("error");
+		Result result = errorObj instanceof Result ? (Result) errorObj : Result.TOKEN_INVALID; // DEFAULT_ERROR는 기본 에러로 설정
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.getWriter().write(objectMapper.writeValueAsString(
+			CustomResponseEntity.builder()
+			.code(401)
+			.message(result.getMessage())
+			.build()));
+	}
+}
