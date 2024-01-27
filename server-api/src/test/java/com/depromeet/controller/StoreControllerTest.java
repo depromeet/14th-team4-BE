fa2f4d34769f709b13b6071afeb048828b534a8f
thisishwan2 <feel000617@gmail.com>
2024-01-27 14:55:43 +0900

    fix: review 삭제 adoc 수정

--- server-api/src/test/java/com/depromeet/controller/StoreControllerTest.java
+++ server-api/src/test/java/com/depromeet/controller/StoreControllerTest.java
@@ -580,4 +580,36 @@ class StoreControllerTest extends RestDocsTestSupport {
 			);
 	}
 
+	@Test
+	void deleteReview() throws Exception {
+		// given
+		Long reviewId = 1L;
+
+		doNothing().when(storeService).deleteStoreReview(any(), eq(reviewId));
+
+		// when
+		mockMvc.perform(
+				delete("/api/v1/reviews/{reviewId}", reviewId)
+					.with(csrf())
+					.contentType(MediaType.APPLICATION_JSON)
+					.header("Authorization", "Bearer accessToken"))
+			.andExpect(status().isOk())
+			.andDo(
+				restDocs.document(
+					pathParameters(
+						parameterWithName("reviewId").description("리뷰 ID")
+					),
+					requestHeaders(
+						headerWithName("Authorization").description("accessToken")
+					),
+					responseFields(
+						fieldWithPath("code").type(JsonFieldType.NUMBER).description("결과코드"),
+						fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
+						fieldWithPath("data").type(JsonFieldType.NULL).description("null")
+					)
+				)
+			);
+
+	}
+
 }
