9cf786c2a113eb48df55915ddef0df39c64507fd
유희수 <soo990315@naver.com>
2024-01-28 01:42:08 +0900

    [feat] 쿠키 삭제 로직 추가 (추후 변경 가능)

--- server-api/src/main/java/com/depromeet/auth/service/CookieService.java
+++ server-api/src/main/java/com/depromeet/auth/service/CookieService.java
@@ -1,9 +1,14 @@
 package com.depromeet.auth.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CookieService {
@@ -13,13 +18,26 @@ public class CookieService {
 	@Value("${jwt.refresh.expiration}")
 	private Long refreshTokenExpirationPeriod;
 
+	public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies != null && cookies.length > 0) {
+			for (Cookie cookie : cookies) {
+				if (name.equals(cookie.getName())) {
+					return Optional.of(cookie);
+				}
+			}
+		}
+		return Optional.empty();
+	}
+
 	public Cookie createAccessTokenCookie(String token) {
 		Cookie cookie = new Cookie("accessToken", token);
 		int accessTokenExpirationInSeconds = (int)(accessTokenExpirationPeriod / 1000);
 		cookie.setMaxAge(accessTokenExpirationInSeconds);
 		cookie.setDomain("ddoeat.site");
 		cookie.setPath("/");
-		cookie.setHttpOnly(true);
+		cookie.setHttpOnly(false);
 		cookie.setSecure(true);
 		return cookie;
 	}
@@ -33,5 +51,25 @@ public class CookieService {
 		cookie.setHttpOnly(true);
 		cookie.setSecure(true);
 		return cookie;
+	}
+
+	public void deleteAccessTokenCookie(HttpServletResponse response) {
+		Cookie cookie = new Cookie("accessToken", null);
+		cookie.setMaxAge(0);
+		cookie.setDomain("ddoeat.site");
+		cookie.setPath("/");
+		cookie.setHttpOnly(false);
+		cookie.setSecure(true);
+		response.addCookie(cookie);
+	}
+
+	public void deleteRefreshTokenCookie(HttpServletResponse response) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setMaxAge(0);
+		cookie.setDomain("ddoeat.site");
+		cookie.setPath("/");
+		cookie.setHttpOnly(true);
+		cookie.setSecure(true);
+		response.addCookie(cookie);
 	}
 }
