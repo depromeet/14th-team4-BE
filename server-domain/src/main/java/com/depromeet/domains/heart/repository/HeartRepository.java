4f435dbeee97ce91098b67e0967c5aedf345bc07
highjune <highjune37@gmail.com>
2024-01-30 22:56:32 +0900

    [fix] 사용 안하는 entity 삭제(follow, heart)

--- server-domain/src/main/java/com/depromeet/domains/heart/repository/HeartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.depromeet.domains.heart.repository;
-
-import com.depromeet.domains.heart.entity.Heart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeartRepository extends JpaRepository<Heart, Long> {
-}
