ca1c5eb2fa7d852f52c513c96fc867487cf2741a
유희수 <soo990315@naver.com>
2024-01-31 02:05:55 +0900

    [fix] 쿼리 변경

--- server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepository.java
+++ server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepository.java
@@ -3,11 +3,14 @@ package com.depromeet.domains.user.repository;
 import com.depromeet.domains.user.entity.User;
 import com.depromeet.enums.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+	@Query("SELECT u FROM User u WHERE u.socialType = :socialType AND u.socialId = :socialId AND u.deletedAt is null")
+	Optional<User> findBySocialTypeAndSocialId(@Param("socialType") SocialType socialType, @Param("socialId") String socialId);
 
 	boolean existsByNickName(String nickname);
 }
