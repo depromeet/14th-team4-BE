f1150d433a909ea849cd2e0a1651655b10507e55
highjune <highjune37@gmail.com>
2024-02-15 00:41:40 +0900

    [fix] viewLevel 하한값 정의

--- server-domain/src/main/java/com/depromeet/enums/ViewLevel.java
+++ server-domain/src/main/java/com/depromeet/enums/ViewLevel.java
@@ -9,19 +9,20 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ViewLevel {
 
-	ONE(1, 1.0),
-	TWO(2, 1.0),
-	THREE(3, 1.0),
-	FOUR(4, 0.9),
-	FIVE(5, 0.8),
-	SIX(6, 0.7),
-	SEVEN(7, 0.6),
-	EIGHT(8, 0.5),
-	NINE(9, 0.4),
-	ETC(-1, 0.0);
+	ONE(1, 1.0, Integer.MAX_VALUE),
+	TWO(2, 1.0, Integer.MAX_VALUE),
+	THREE(3, 1.0, Integer.MAX_VALUE),
+	FOUR(4, 0.9, 20),
+	FIVE(5, 0.8, 20),
+	SIX(6, 0.7, 20),
+	SEVEN(7, 0.6, 15),
+	EIGHT(8, 0.5, 15),
+	NINE(9, 0.4, 15),
+	ETC(-1, 0.0, Integer.MAX_VALUE);
 
 	private final int level;
 	private final double ratio;
+	private final int minCount;
 
 	public static ViewLevel findByLevel(int level) {
 		return Arrays.stream(ViewLevel.values())
