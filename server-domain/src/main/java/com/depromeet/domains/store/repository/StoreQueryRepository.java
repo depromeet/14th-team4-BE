42adf3240b09dc2b3074df2264ad32997c0c1152
highjune <highjune37@gmail.com>
2024-03-04 20:03:45 +0900

    [feat] StorequeryRepository에 의존성 주입 추가

--- server-domain/src/main/java/com/depromeet/domains/store/repository/StoreQueryRepository.java
+++ server-domain/src/main/java/com/depromeet/domains/store/repository/StoreQueryRepository.java
@@ -1,18 +1,28 @@
 package com.depromeet.domains.store.repository;
 
+import static com.depromeet.domains.store.entity.QStore.*;
+
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import com.depromeet.domains.store.entity.Store;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
 
 @Repository
+@AllArgsConstructor
 public class StoreQueryRepository {
 
 	private final JPAQueryFactory queryFactory;
 
-	public StoreQueryRepository(EntityManager entityManager) {
-		this.queryFactory = new JPAQueryFactory(entityManager);
+	// 테스트 (추후 지우기)
+	public List<Store> getStoreAll() {
+		return queryFactory
+			.select(store)
+			.from(store)
+			.fetch();
 	}
 
 }
