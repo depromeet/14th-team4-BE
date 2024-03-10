a430366210d1792294569392f578ca7a1ed03500
유희수 <soo990315@naver.com>
2024-03-10 23:01:21 +0900

    [refactor] user-existsBy querydsl 변경

--- server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepositoryCustom.java
+++ server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepositoryCustom.java
@@ -1,10 +1,7 @@
 package com.depromeet.domains.user.repository;
 
-import java.awt.print.Pageable;
-import org.springframework.data.domain.Slice;
-import com.depromeet.domains.bookmark.entity.Bookmark;
 
 public interface UserRepositoryCustom {
-	Slice<Bookmark> findByUser(Long userId, Pageable pageable);
 
+	boolean existsByNickName(String nickname);
 }
--- server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepositoryImpl.java
+++ server-domain/src/main/java/com/depromeet/domains/user/repository/UserRepositoryImpl.java
@@ -1,14 +1,11 @@
 package com.depromeet.domains.user.repository;
 
-import java.awt.print.Pageable;
-
-import com.depromeet.domains.bookmark.entity.Bookmark;
-import com.depromeet.domains.bookmark.entity.QBookmark;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
+
+import com.depromeet.domains.user.entity.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,7 +13,13 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Slice<Bookmark> findByUser(Long userId, Pageable pageable) {
-        return null;
+    public boolean existsByNickName(String nickname) {
+        QUser user = QUser.user;
+        Integer result = jpaQueryFactory
+            .selectOne()
+            .from(user)
+            .where(user.nickName.eq(nickname))
+            .fetchFirst();
+        return result != null;
     }
 }
