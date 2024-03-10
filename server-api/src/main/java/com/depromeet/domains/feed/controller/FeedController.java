b5d04d6fa858f819fd2dad85710476f38e8d62f3
thisishwan2 <feel000617@gmail.com>
2024-03-10 13:10:44 +0900

    feat: feed 컨트롤러, 서비스 추가

--- server-api/src/main/java/com/depromeet/domains/feed/controller/FeedController.java
+++ server-api/src/main/java/com/depromeet/domains/feed/controller/FeedController.java
@@ -1,10 +1,18 @@
 package com.depromeet.domains.feed.controller;
 
+import com.depromeet.annotation.AuthUser;
+import com.depromeet.common.exception.CustomResponseEntity;
 import com.depromeet.domains.feed.service.FeedService;
+import com.depromeet.domains.store.dto.response.StoreReviewResponse;
+import com.depromeet.domains.user.entity.User;
+import com.depromeet.enums.ReviewType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RequestMapping("/api/v1/feeds")
@@ -13,4 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedController {
 
     private final FeedService feedService;
+
+    // 피드 전체 조회
+
+    // 피드 상세 조회
+
+    // 피드 작성
+
+    // 피드 수정
+
+    // 피드 삭제
+    @DeleteMapping("/{feedId}")
+    public CustomResponseEntity<Void> deleteStoreReview(@AuthUser User user,
+                                                        @PathVariable Long feedId) {
+        feedService.deleteFeed(user, feedId);
+        return CustomResponseEntity.success();
+    }
 }
--- server-api/src/main/java/com/depromeet/domains/feed/service/FeedService.java
+++ server-api/src/main/java/com/depromeet/domains/feed/service/FeedService.java
@@ -1,9 +1,16 @@
 package com.depromeet.domains.feed.service;
 
+import com.depromeet.common.exception.CustomException;
+import com.depromeet.common.exception.Result;
+import com.depromeet.domains.feed.entity.Feed;
 import com.depromeet.domains.feed.repository.FeedRepository;
+import com.depromeet.domains.store.entity.Store;
+import com.depromeet.domains.store.repository.StoreRepository;
+import com.depromeet.domains.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +18,27 @@ import org.springframework.stereotype.Service;
 public class FeedService {
 
     private final FeedRepository feedRepository;
+    private final StoreRepository storeRepository;
+
+    // 피드 전체 조회
+
+    // 피드 상세 조회
+
+    // 피드 작성
+
+    // 피드 수정
+
+    // 피드 삭제
+    @Transactional
+    public void deleteFeed(User user, Long feedId) {
+
+        Feed feed = feedRepository.findById(feedId).orElseThrow(() -> new CustomException(Result.NOT_FOUND_FEED));
+        Store store = storeRepository.findById(feed.getStoreId()).orElseThrow(() -> new CustomException(Result.NOT_FOUND_STORE));
+
+        // 음식점 별점 감소, 사용자 피드 수 감소
+        store.decreaseTotalRating(feed.getRating());
+        user.decreaseMyFeedCount();
+
+        feedRepository.delete(feed);
+    }
 }
