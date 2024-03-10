169c47be52ba31043ffe9ade086615c67a0ae803
highjune <highjune37@gmail.com>
2024-03-10 20:17:13 +0900

    [fix] Feed Repository 수정(Review -> Feed)

--- server-domain/src/main/java/com/depromeet/domains/feed/repository/FeedRepository.java
+++ server-domain/src/main/java/com/depromeet/domains/feed/repository/FeedRepository.java
@@ -1,10 +1,12 @@
 package com.depromeet.domains.feed.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.depromeet.domains.feed.entity.Feed;
 import org.springframework.data.domain.Pageable;
 
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,7 +17,7 @@ import com.depromeet.domains.user.entity.User;
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom {
 
 	//리뷰 이미지가 존재하는 가장 최근 10개의 리뷰 조회
-	List<Review> findTop10ByStoreOrderByVisitedAtDesc(Store store);
+	List<Feed> findTop10ByStoreOrderByVisitedAtDesc(Store store);
 
 	// 자신이 특정 음식점에 몇번째 재방문인지 조회
 	Long countByStoreAndUser(Store store, User user);
@@ -32,23 +34,23 @@ public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositor
 	@Query("SELECT r FROM Review r WHERE r.store = :store AND r.user IN " +
 		"(SELECT u.user FROM Review u WHERE u.store = :store GROUP BY u.user HAVING COUNT(u) >= 2) " +
 		"ORDER BY r.visitedAt DESC")
-	Slice<Review> findRevisitedReviews(@Param("store") Store store, Pageable pageable);
+	Slice<Feed> findRevisitedReviews(@Param("store") Store store, Pageable pageable);
 
 	// 사진 리뷰만 조회
-	Slice<Review> findByStoreAndImageUrlIsNotNullOrderByVisitedAtDesc(Store store, Pageable pageable);
+	Slice<Feed> findByStoreAndImageUrlIsNotNullOrderByVisitedAtDesc(Store store, Pageable pageable);
 
 	boolean existsByStoreAndUser(Store store, User user);
 
-	Slice<Review> findByUser(User user, Pageable pageable);
+	Slice<Feed> findByUser(User user, Pageable pageable);
 
-	List<Review> findByUser(User user);
+	List<Feed> findByUser(User user);
 
-	List<Review> findByStore(Store store);
+	List<Feed> findByStore(Store store);
 
 	@Query(value = "SELECT COUNT(*) FROM (SELECT COUNT(reviewId) AS review_count FROM Review WHERE store_id = :storeId GROUP BY user_id) AS subquery WHERE review_count = :count", nativeQuery = true)
 	Long countByStoreAndReviewCount(@Param("storeId") Long storeId, @Param("count") Long count);
 
-	Slice<Review> findByStore(Store store, Pageable pageable);
+	Slice<Feed> findByStore(Store store, Pageable pageable);
 
 	@Query("SELECT COUNT(r) FROM Review r WHERE r.store = :store AND r.user = :user AND r.createdAt BETWEEN :startOfDay AND :endOfDay")
 	int countStoreReviewByUserForDay(@Param("user") User user, @Param("store") Store store,
