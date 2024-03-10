428084d7fe1d2cf93d7cebceebf092511d90bd5b
유희수 <soo990315@naver.com>
2024-03-10 23:02:14 +0900

    [refactor] user,bookmark response 변경

--- server-api/src/main/java/com/depromeet/domains/user/dto/response/UserBookmarkResponse.java
+++ server-api/src/main/java/com/depromeet/domains/user/dto/response/UserBookmarkResponse.java
@@ -11,18 +11,17 @@ public class UserBookmarkResponse {
 	private String storeName; // 북마크한 가게 이름
 	private String address; // 북마크한 가게 주소
 	private Long totalRevisitedCount; // 북마크한 가게에 총 몇명이 재방문했는지
-	private String categoryName; // 북마크한 가게의 카테고리
+	private String kakaoCategoryName; // 북마크한 가게의 카테고리
 	private Boolean isVisited; // 북마크한 가게에 내가 방문했는지 안했는지
 
-	public static UserBookmarkResponse of(Long bookmarkId, Long storeId, String storeName, String address, Long totalRevisitedCount,
-		String categoryName, Boolean isVisited) {
+	public static UserBookmarkResponse of(Long bookmarkId, Long storeId, String storeName, String address,
+		String kakaoCategoryName, Boolean isVisited) {
 		return UserBookmarkResponse.builder()
 			.bookmarkId(bookmarkId)
 			.storeId(storeId)
 			.storeName(storeName)
 			.address(address)
-			.totalRevisitedCount(totalRevisitedCount)
-			.categoryName(categoryName)
+			.kakaoCategoryName(kakaoCategoryName)
 			.isVisited(isVisited)
 			.build();
 	}
