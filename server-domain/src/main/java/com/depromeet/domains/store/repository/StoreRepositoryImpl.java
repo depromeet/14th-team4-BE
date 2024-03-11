3058bb8ae465ad65d38d06273e66a19b07096bfc
kangjune <kangjune@vendys.co.kr>
2024-03-11 23:36:10 +0900

    [fix] Store 쪽 queryDsl 정의

--- server-domain/src/main/java/com/depromeet/domains/store/repository/StoreRepositoryImpl.java
+++ server-domain/src/main/java/com/depromeet/domains/store/repository/StoreRepositoryImpl.java
@@ -4,26 +4,51 @@ import static com.depromeet.domains.store.entity.QStore.*;
 
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import com.depromeet.domains.store.entity.Store;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class StoreRepositoryImpl implements StoreRepositoryCustom{
+public class StoreRepositoryImpl implements StoreRepositoryCustom {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
-	// 테스트 (추후 지우기)
-	public List<Store> getStoreAll() {
-		return jpaQueryFactory
-			.select(store)
+	@Override
+	public List<Store> findByLocationRangesNotInBookmarks(double maxLatitude, double minLatitude, double maxLongitude,
+		double minLongitude, List<Long> storeIdList) {
+
+		return jpaQueryFactory.select(store)
 			.from(store)
+			.where(searchLocationRangesWithBookmarks(
+				maxLatitude, minLatitude, maxLongitude, minLongitude, storeIdList
+			))
 			.fetch();
+	}
+
+	@Override
+	public List<Store> findByStoreIdList(List<Long> storeIdList) {
+		return jpaQueryFactory.select(store)
+			.from(store)
+			.where(store.storeId.in(storeIdList))
+			.fetch();
+	}
+
+	private BooleanExpression searchLocationRangesWithBookmarks(double maxLatitude, double minLatitude, double maxLongitude,
+		double minLongitude, List<Long> storeIdList) {
+		BooleanExpression condition = store.location.latitude.loe(maxLatitude)
+			.and(store.location.latitude.goe(minLatitude))
+			.and(store.location.longitude.loe(maxLongitude))
+			.and(store.location.longitude.goe(minLongitude));
+
+		if (storeIdList.size() == 0) {
+			condition = condition.and(store.storeId.notIn(storeIdList));
+		}
+		return condition;
 	}
 
 }
