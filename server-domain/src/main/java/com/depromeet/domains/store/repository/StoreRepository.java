fd53a6d5c91e837f4bfedfe062543c5e767fa6d3
kangjune <kangjune@vendys.co.kr>
2024-03-11 23:34:10 +0900

    [fix] StoreRepository 에서 이전 함수들 삭제

--- server-domain/src/main/java/com/depromeet/domains/store/repository/StoreRepository.java
+++ server-domain/src/main/java/com/depromeet/domains/store/repository/StoreRepository.java
@@ -8,48 +8,6 @@ import com.depromeet.domains.store.entity.Store;
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
 
-	// todo - to queryDsl
-	// @Query("SELECT s "
-	// 	+ "      FROM Store s "
-	// 	+ "      LEFT JOIN FETCH s.storeMeta sm "
-	// 	+ "  WHERE s.location.latitude <= :maxLatitude "
-	// 	+ " 	  AND s.location.latitude >= :minLatitude "
-	// 	+ " 	  AND s.location.longitude <= :maxLongitude "
-	// 	+ " 	  AND s.location.longitude >= :minLongitude "
-	// 	+ " 	  AND (s.category.categoryType = :categoryType or :categoryType is null) "
-	// 	+ " 	  AND s.storeId NOT IN (:storeIdList) "
-	// 	+ "    ORDER BY sm.totalRevisitedCount DESC ")
-	// List<Store> findByLocationRangesWithCategory(
-	// 	@Param("maxLatitude") double maxLatitude,
-	// 	@Param("minLatitude") double minLatitude,
-	// 	@Param("maxLongitude") double maxLongitude,
-	// 	@Param("minLongitude") double minLongitude,
-	// 	@Param("categoryType") CategoryType categoryType,
-	// 	@Param("storeIdList") List<Long> storeIdList);
-
-	// todo - to queryDsl
-	// @Query("SELECT s "
-	// 	+ "      FROM Store s "
-	// 	+ "      LEFT JOIN FETCH s.storeMeta sm "
-	// 	+ "  WHERE s.location.latitude <= :maxLatitude "
-	// 	+ " 	  AND s.location.latitude >= :minLatitude "
-	// 	+ " 	  AND s.location.longitude <= :maxLongitude "
-	// 	+ " 	  AND s.location.longitude >= :minLongitude "
-	// 	+ " 	  AND (s.category.categoryType = :categoryType or :categoryType is null) "
-	// 	+ "    ORDER BY sm.totalRevisitedCount DESC ")
-	// List<Store> findByLocationRangesWithCategoryNoExcept(
-	// 	@Param("maxLatitude") double maxLatitude,
-	// 	@Param("minLatitude") double minLatitude,
-	// 	@Param("maxLongitude") double maxLongitude,
-	// 	@Param("minLongitude") double minLongitude,
-	// 	@Param("categoryType") CategoryType categoryType);
-
-	// @Query(" SELECT s "
-	// 	+ "      FROM Bookmark bm "
-	// 	+ "      LEFT JOIN bm.store s "
-	// 	+ "    WHERE bm.user.userId = :userId ")
-	// List<Store> findByUsersBookMarkList(@Param("userId") Long userId);
-
 	Boolean existsByStoreNameAndAddress(String storeName, String address);
 
 	Store findByKakaoStoreId(Long kakaoStoreId);
