5f364cc56e3464f653e78424be440e86a4730c69
highjune <highjune37@gmail.com>
2024-03-18 22:33:28 +0900

    [feat] FollowRepositoryCustom 정의

--- server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
+++ server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
@@ -1,4 +1,7 @@
 package com.depromeet.domains.follow.repository;
 
 public interface FollowRepositoryCustom {
+
+    Long getFollowingCountBySenderId(Long senderId);
+    Long getFollowerCountByReceiverId(Long receiverId);
 }
