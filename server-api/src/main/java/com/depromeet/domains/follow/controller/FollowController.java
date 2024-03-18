b15b05dcb4a421ce97a411f5d032b792e644182e
유희수 <soo990315@naver.com>
2024-03-18 19:58:59 +0900

    [feat] 팔로잉/언팔로잉 요청 API 추가

--- server-api/src/main/java/com/depromeet/domains/follow/controller/FollowController.java
+++ server-api/src/main/java/com/depromeet/domains/follow/controller/FollowController.java
@@ -1,11 +1,31 @@
 package com.depromeet.domains.follow.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1")
+import com.depromeet.annotation.AuthUser;
+import com.depromeet.common.exception.CustomResponseEntity;
+import com.depromeet.domains.bookmark.dto.response.BookmarkingResponse;
+import com.depromeet.domains.bookmark.service.BookmarkService;
+import com.depromeet.domains.follow.dto.FollowUpdateResponse;
+import com.depromeet.domains.follow.service.FollowService;
+import com.depromeet.domains.user.entity.User;
+
+@RequestMapping("/api/v1/follows")
 @RestController
 @RequiredArgsConstructor
 public class FollowController {
+	private final FollowService followService;
+
+	// 팔로잉 등록, 팔로잉 취소 (팔로잉할, 팔로잉 취소할 user id)
+	@PatchMapping("/{receiverId}")
+	public CustomResponseEntity<FollowUpdateResponse> updateFollow (@AuthUser User user, @PathVariable Long receiverId) {
+		return CustomResponseEntity.success(followService.updateFollow(user, receiverId));
+	}
+
+	// 팔로우 목록 조회
 }
---
+++ server-api/src/main/java/com/depromeet/domains/follow/service/FollowService.java
@@ -0,0 +1,44 @@
+package com.depromeet.domains.follow.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.depromeet.common.exception.CustomException;
+import com.depromeet.common.exception.Result;
+import com.depromeet.domains.follow.dto.FollowUpdateResponse;
+import com.depromeet.domains.follow.entity.Follow;
+import com.depromeet.domains.follow.repository.FollowRepository;
+import com.depromeet.domains.user.entity.User;
+import com.depromeet.domains.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FollowService {
+
+	private final FollowRepository followRepository;
+	private final UserRepository userRepository;
+
+	public FollowUpdateResponse updateFollow(User user, Long receiverId) {
+		User receiver = userRepository.findById(receiverId)
+			.orElseThrow(() -> new CustomException(Result.NOT_FOUND_USER));
+
+		Optional<Follow> follow = followRepository.findBySenderIdAndReceiverId(user.getUserId(), receiver.getUserId());
+		if (follow.isPresent()) {
+			followRepository.delete(follow.get());
+			return FollowUpdateResponse.of(follow.get().getFollowId(), user.getUserId(), receiver.getUserId(), false);
+		}
+
+		// 팔로우 관계가 없으면 새로 생성 (팔로우)
+		Follow newFollow = Follow.builder()
+			.senderId(user.getUserId())
+			.receiverId(receiver.getUserId())
+			.build();
+		followRepository.save(newFollow);
+		return FollowUpdateResponse.of(newFollow.getFollowId(), user.getUserId(), receiver.getUserId(), true);
+	}
+}
