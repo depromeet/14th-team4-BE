d5105dc01bf94e7e05d6a2da8cc2a6edf8b31a93
유희수 <soo990315@naver.com>
2024-03-19 01:55:20 +0900

    [feat] 동적 쿼리 적용 X

--- server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
+++ server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.depromeet.domains.follow.repository;
 
+import java.util.List;
+
+import com.depromeet.domains.follow.dto.FollowListResponse;
+
 public interface FollowRepositoryCustom {
+	// 팔로우 목록 조회
+	List<FollowListResponse> findFollowersWithFollowStatus(Long userId, Long targetUserId);
+	List<FollowListResponse> findFollowingsWithFollowStatus(Long userId, Long targetUserId);
 }
--- server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryImpl.java
+++ server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryImpl.java
@@ -1,4 +1,76 @@
 package com.depromeet.domains.follow.repository;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.depromeet.domains.follow.dto.FollowListResponse;
+import com.depromeet.domains.follow.entity.QFollow;
+import com.depromeet.domains.user.entity.QUser;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class FollowRepositoryImpl implements FollowRepositoryCustom{
+	private final JPAQueryFactory jpaQueryFactory;
+	QFollow follow = QFollow.follow;
+	QUser user = QUser.user;
+	QFollow subFollow = new QFollow("subFollow");
+
+	@Override
+	public List<FollowListResponse> findFollowersWithFollowStatus(Long userId, Long targetUserId) {
+		List<FollowListResponse> followers = jpaQueryFactory
+			.select(Projections.constructor(FollowListResponse.class,
+				follow.senderId,
+				user.nickName,
+				user.profileImageUrl,
+				new CaseBuilder()
+					.when(JPAExpressions
+						.selectOne()
+						.from(subFollow)
+						.where(subFollow.senderId.eq(userId)
+							.and(subFollow.receiverId.eq(follow.senderId)))
+						.exists())
+					.then(true)
+					.otherwise(false)
+			))
+			.from(follow)
+			.join(user).on(follow.senderId.eq(user.userId))
+			.where(follow.receiverId.eq(targetUserId)
+				.and(follow.senderId.ne(userId))) // 자기 자신은 제외
+			.fetch();
+
+		return followers;
+	}
+
+	@Override
+	public List<FollowListResponse> findFollowingsWithFollowStatus(Long userId, Long targetUserId) {
+		List<FollowListResponse> followings = jpaQueryFactory
+			.select(Projections.constructor(FollowListResponse.class,
+				follow.receiverId,
+				user.nickName,
+				user.profileImageUrl,
+				new CaseBuilder()
+					.when(JPAExpressions
+						.selectOne()
+						.from(subFollow)
+						.where(subFollow.senderId.eq(userId)
+							.and(subFollow.receiverId.eq(follow.receiverId)))
+						.exists())
+					.then(true)
+					.otherwise(false)
+			))
+			.from(follow)
+			.join(user).on(follow.receiverId.eq(user.userId))
+			.where(follow.senderId.eq(targetUserId)
+				.and(follow.receiverId.ne(userId))) // 자기 자신은 제외
+			.fetch();
+
+		return followings;
+	}
 }
