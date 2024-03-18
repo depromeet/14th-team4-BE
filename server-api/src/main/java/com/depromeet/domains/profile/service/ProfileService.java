1d1ab88ea5ddd436c002940bf16b8b57f8490ad4
highjune <highjune37@gmail.com>
2024-03-18 22:34:14 +0900

    [feat] ProfileService 정의

--- server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
+++ server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
@@ -1,24 +1,22 @@
 package com.depromeet.domains.profile.service;
 
-import com.depromeet.common.exception.CustomException;
-import com.depromeet.common.exception.Result;
 import com.depromeet.domains.follow.repository.FollowRepository;
 import com.depromeet.domains.profile.dto.response.ProfileResponse;
 import com.depromeet.domains.user.entity.User;
-import com.depromeet.domains.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ProfileService {
 
-    private final UserRepository userRepository;
     private final FollowRepository followRepository;
 
     public ProfileResponse getProfileInfo(User user, boolean isMine) {
+        Long followingCnt = followRepository.getFollowingCountBySenderId(user.getUserId());
+        Long followerCnt = followRepository.getFollowerCountByReceiverId(user.getUserId());
 
+        return ProfileResponse.of(isMine, user.getUserId(), user.getProfileImageUrl()
+                , user.getNickName(), user.getMyFeedCnt(), followerCnt, followingCnt, false);
     }
 }
