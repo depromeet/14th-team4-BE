bc64b00dce6d10b3e98ccf0eb42ea8131e12d991
highjune <highjune37@gmail.com>
2024-03-18 22:33:43 +0900

    [feat] FollowRepositoryImpl 구현

--- server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryImpl.java
+++ server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryImpl.java
@@ -1,4 +1,46 @@
 package com.depromeet.domains.follow.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static com.depromeet.domains.follow.entity.QFollow.follow;
+
+
+@Repository
+@RequiredArgsConstructor
 public class FollowRepositoryImpl implements FollowRepositoryCustom{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Long getFollowingCountBySenderId(Long senderId) {
+        return jpaQueryFactory
+                .select(follow.count())
+                .from(follow)
+                .where(
+                        senderIdEq(senderId)
+                )
+                .fetchOne();
+    }
+
+    @Override
+    public Long getFollowerCountByReceiverId(Long receiverId) {
+        return jpaQueryFactory
+                .select(follow.count())
+                .from(follow)
+                .where(
+                        receiverIdEq(receiverId)
+                )
+                .fetchOne();
+    }
+
+    private BooleanExpression senderIdEq(Long senderId) {
+        return follow.senderId.eq(senderId);
+    }
+
+    private BooleanExpression receiverIdEq(Long receiverId) {
+        return follow.receiverId.eq(receiverId);
+    }
 }
