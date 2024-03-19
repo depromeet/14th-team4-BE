49763ca23544eb0a8c1d39ee77c18f5c52cae4a0
highjune <highjune37@gmail.com>
2024-03-19 20:13:33 +0900

    [feat] getFollowByEachId 함수 정의

--- server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
+++ server-domain/src/main/java/com/depromeet/domains/follow/repository/FollowRepositoryCustom.java
@@ -1,7 +1,13 @@
 package com.depromeet.domains.follow.repository;
 
+import com.depromeet.domains.follow.entity.Follow;
+
+import java.util.List;
+
 public interface FollowRepositoryCustom {
 
     Long getFollowingCountBySenderId(Long senderId);
     Long getFollowerCountByReceiverId(Long receiverId);
+
+    List<Follow> getFollowByEachId(Long senderId, Long receiverId);
 }
