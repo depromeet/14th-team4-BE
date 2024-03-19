d023d0e40ab14e926a3a2ccf35c1d8ac765921f2
highjune <highjune37@gmail.com>
2024-03-19 20:50:53 +0900

    [fix] getProfileInfo -> getProfile 함수명 수정, loginUser -> profileUser 변경

--- server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
+++ server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
@@ -1,8 +1,11 @@
 package com.depromeet.domains.profile.service;
 
+import com.depromeet.common.exception.CustomException;
+import com.depromeet.common.exception.Result;
 import com.depromeet.domains.follow.repository.FollowRepository;
 import com.depromeet.domains.profile.dto.response.ProfileResponse;
 import com.depromeet.domains.user.entity.User;
+import com.depromeet.domains.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -12,15 +15,19 @@ import org.springframework.util.ObjectUtils;
 public class ProfileService {
 
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
 
-    public ProfileResponse getProfileInfo(User loginUser, Long profileUserId) {
+    public ProfileResponse getProfile(User loginUser, Long profileUserId) {
         Long followingCnt = followRepository.getFollowingCountBySenderId(loginUser.getUserId());
         Long followerCnt = followRepository.getFollowerCountByReceiverId(loginUser.getUserId());
-        boolean isMine = loginUser.getUserId().equals(profileUserId);
+        User profileUser = userRepository.findById(profileUserId)
+                .orElseThrow(() -> new CustomException(Result.NOT_FOUND_USER));
+
+        boolean isMine = loginUser.equals(profileUser);
         boolean isFollowed = isUserFollowingMe(loginUser, profileUserId);
 
-        return ProfileResponse.of(isMine, loginUser.getUserId(), loginUser.getProfileImageUrl()
-                , loginUser.getNickName(), loginUser.getMyFeedCnt(), followerCnt, followingCnt, isFollowed);
+        return ProfileResponse.of(isMine, profileUser.getUserId(), profileUser.getProfileImageUrl()
+                , profileUser.getNickName(), profileUser.getMyFeedCnt(), followerCnt, followingCnt, isFollowed);
     }
 
     private boolean isUserFollowingMe(User loginUser, Long profileUserId) {
