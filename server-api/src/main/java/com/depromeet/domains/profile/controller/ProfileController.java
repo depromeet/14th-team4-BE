7e652a743ea09c41831063f76fea89180e093cb5
highjune <highjune37@gmail.com>
2024-03-19 20:50:09 +0900

    [fix] getProfileInfo -> getProfile 수정

--- server-api/src/main/java/com/depromeet/domains/profile/controller/ProfileController.java
+++ server-api/src/main/java/com/depromeet/domains/profile/controller/ProfileController.java
@@ -5,7 +5,6 @@ import com.depromeet.common.exception.CustomResponseEntity;
 import com.depromeet.domains.profile.dto.response.ProfileResponse;
 import com.depromeet.domains.profile.service.ProfileService;
 import com.depromeet.domains.user.entity.User;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +19,9 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @GetMapping("/{userId}")
-    public CustomResponseEntity<ProfileResponse> getProfileInfo(@PathVariable("userId") Long userId,
-                                                                @AuthUser User user) {
+    public CustomResponseEntity<ProfileResponse> getProfile(@PathVariable("userId") Long userId,
+                                                            @AuthUser User user) {
         return CustomResponseEntity.success(
-                this.profileService.getProfileInfo(user, userId));
+                this.profileService.getProfile(user, userId));
     }
 }
