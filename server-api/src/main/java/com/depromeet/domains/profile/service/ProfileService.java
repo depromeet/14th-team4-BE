6eaec87cb82b3806b001c4e8a1eeb3baae3e9e51
kangjune <kangjune@vendys.co.kr>
2024-03-22 20:31:52 +0900

    [feat] 특정 유저 피드 조회api service 추가

--- server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
+++ server-api/src/main/java/com/depromeet/domains/profile/service/ProfileService.java
@@ -2,13 +2,22 @@ package com.depromeet.domains.profile.service;
 
 import com.depromeet.common.exception.CustomException;
 import com.depromeet.common.exception.Result;
+import com.depromeet.domains.feed.repository.FeedRepository;
+import com.depromeet.domains.feed.repository.ProfileFeedProjection;
 import com.depromeet.domains.follow.repository.FollowRepository;
 import com.depromeet.domains.profile.dto.response.ProfileResponse;
+import com.depromeet.domains.store.dto.StoreFeedResponse;
+import com.depromeet.domains.store.repository.StoreRepository;
 import com.depromeet.domains.user.entity.User;
 import com.depromeet.domains.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+
+import static com.depromeet.common.CursorPagingCommon.getSlice;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +25,8 @@ public class ProfileService {
 
     private final FollowRepository followRepository;
     private final UserRepository userRepository;
+    private final FeedRepository feedRepository;
+    private final StoreRepository storeRepository;
 
     public ProfileResponse getProfile(User loginUser, Long profileUserId) {
         Long followingCnt = followRepository.getFollowingCountBySenderId(loginUser.getUserId());
@@ -36,5 +47,14 @@ public class ProfileService {
         }
 
         return !ObjectUtils.isEmpty(followRepository.getFollowByEachId(profileUserId, loginUser.getUserId()));
+    }
+
+    public Slice<ProfileResponse> getProfileFeed(User loginUser, Long profileUserId, Long lastIdxId, Integer size) {
+        User profileUser = userRepository.findById(profileUserId)
+                .orElseThrow(() -> new CustomException(Result.NOT_FOUND_USER));
+
+        List<ProfileFeedProjection> profileFeedList
+                            = feedRepository.findProfileFeed(loginUser, profileUser.getUserId(), lastIdxId, size);
+        return getSlice(profileFeedList, size);
     }
 }
