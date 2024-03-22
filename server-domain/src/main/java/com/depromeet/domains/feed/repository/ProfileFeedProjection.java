c5b22878f169a6913ca2c1032aca2157324f4041
kangjune <kangjune@vendys.co.kr>
2024-03-22 20:57:08 +0900

    [feat] ProfileFeedProject 객체 추가

---
+++ server-domain/src/main/java/com/depromeet/domains/feed/repository/ProfileFeedProjection.java
@@ -0,0 +1,41 @@
+package com.depromeet.domains.feed.repository;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProfileFeedProjection {
+
+    private Long feedId;
+    private Long userId;
+    private Long storeId;
+    private String storeName;
+    private Long kakaoStoreId;
+    // todo - 위치(행정구)
+    private String feedImageUrl;
+    private LocalDateTime feedCreatedAt;
+    private Long likeCnt;
+    private Long commentCnt;
+    private Boolean isHeartFeed;
+
+    @QueryProjection
+    public ProfileFeedProjection(Long feedId, Long userId, Long storeId, String storeName, Long kakaoStoreId,
+                                 String feedImageUrl, LocalDateTime feedCreatedAt,
+                                 Long likeCnt, Long commentCnt, Boolean isHeartFeed) {
+        this.feedId = feedId;
+        this.userId = userId;
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.kakaoStoreId = kakaoStoreId;
+        this.feedImageUrl = feedImageUrl;
+        this.feedCreatedAt = feedCreatedAt;
+        this.likeCnt = likeCnt;
+        this.commentCnt = commentCnt;
+        this.isHeartFeed = isHeartFeed;
+    }
+}
