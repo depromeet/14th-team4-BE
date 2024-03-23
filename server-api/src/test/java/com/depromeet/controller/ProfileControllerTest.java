a0ecd681605bd181f7fcca3b99a6274691cd87b8
kangjune <kangjune@vendys.co.kr>
2024-03-23 13:48:39 +0900

    [test] Request 객체 수정

--- server-api/src/test/java/com/depromeet/controller/ProfileControllerTest.java
+++ server-api/src/test/java/com/depromeet/controller/ProfileControllerTest.java
@@ -1,10 +1,9 @@
 package com.depromeet.controller;
 
 import com.depromeet.document.RestDocsTestSupport;
+import com.depromeet.domains.profile.dto.request.ProfileNicknameRequest;
 import com.depromeet.domains.profile.dto.response.ProfileFeedResponse;
 import com.depromeet.domains.profile.dto.response.ProfileResponse;
-import com.depromeet.domains.store.dto.StoreFeedResponse;
-import com.depromeet.domains.user.dto.request.NicknameRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -125,57 +124,57 @@ class ProfileControllerTest extends RestDocsTestSupport {
                         .param("size", "10"))
                 .andExpect(status().isOk())
                 .andDo(
-                    restDocs.document(
-                        queryParameters(
-                                parameterWithName("lastIdxId").description("마지막 원소 ID"),
-                                parameterWithName("size").description("페이지 크기")
-                        ),
-                        pathParameters(
-                                parameterWithName("userId").description("프로필 유저id")
-                        ),
-                        requestHeaders(
-                                headerWithName("Authorization").description("accessToken")
-                        ),
-                        responseFields(
-                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("결과코드"),
-                                fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
-                                subsectionWithPath("data.content[]").type(JsonFieldType.ARRAY).description("리뷰 목록"),
-                                fieldWithPath("data.content[].feedId").type(JsonFieldType.NUMBER).description("피드 ID"),
-                                fieldWithPath("data.content[].userId").type(JsonFieldType.NUMBER).description("프로필 유저 ID"),
-                                fieldWithPath("data.content[].storeId").type(JsonFieldType.NUMBER).description("식당 고유 Id"),
-                                fieldWithPath("data.content[].storeName").type(JsonFieldType.STRING).description("식담명"),
-                                fieldWithPath("data.content[].kakaoStoreId").type(JsonFieldType.NUMBER).description("식당 카카오 고유 ID"),
-                                fieldWithPath("data.content[].feedImageUrl").type(JsonFieldType.STRING).description("피드 image URL"),
-                                fieldWithPath("data.content[].feedCreatedAt").type(JsonFieldType.STRING).description("피드 작성 시기"),
-                                fieldWithPath("data.content[].likeCnt").type(JsonFieldType.NUMBER).description("좋아요 수"),
-                                fieldWithPath("data.content[].commentCnt").type(JsonFieldType.NUMBER).description("댓글 수"),
-                                fieldWithPath("data.content[].isHeartFeed").type(JsonFieldType.BOOLEAN).description("좋아요 여부"),
-                                subsectionWithPath("data.pageable").type(JsonFieldType.OBJECT).description("페이지 정보"),
-                                fieldWithPath("data.pageable.pageNumber").type(JsonFieldType.NUMBER).description("페이지 번호"),
-                                fieldWithPath("data.pageable.pageSize").type(JsonFieldType.NUMBER).description("페이지 크기"),
-                                subsectionWithPath("data.pageable.sort").type(JsonFieldType.OBJECT).description("정렬 정보"),
-                                fieldWithPath("data.pageable.sort.empty").type(JsonFieldType.BOOLEAN).description("정렬 정보가 비어 있는지 여부"),
-                                fieldWithPath("data.pageable.sort.sorted").type(JsonFieldType.BOOLEAN).description("정렬이 되어 있는지 여부"),
-                                fieldWithPath("data.pageable.sort.unsorted").type(JsonFieldType.BOOLEAN).description("정렬이 되어 있지 않은지 여부"),
-                                fieldWithPath("data.pageable.offset").type(JsonFieldType.NUMBER).description("페이지 오프셋"),
-                                fieldWithPath("data.pageable.paged").type(JsonFieldType.BOOLEAN).description("페이지가 페이징 되어 있는지 여부"),
-                                fieldWithPath("data.pageable.unpaged").type(JsonFieldType.BOOLEAN).description("페이지가 페이징 되어 있지 않은지 여부"),
-                                fieldWithPath("data.first").type(JsonFieldType.BOOLEAN).description("첫 번째 페이지 여부"),
-                                fieldWithPath("data.last").type(JsonFieldType.BOOLEAN).description("마지막 페이지 여부"),
-                                fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("페이지 크기"),
-                                fieldWithPath("data.number").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
-                                subsectionWithPath("data.sort").type(JsonFieldType.OBJECT).description("정렬 정보"),
-                                fieldWithPath("data.numberOfElements").type(JsonFieldType.NUMBER).description("현재 페이지의 요소 수"),
-                                fieldWithPath("data.empty").type(JsonFieldType.BOOLEAN).description("현재 페이지가 비어 있는지 여부")
+                        restDocs.document(
+                                queryParameters(
+                                        parameterWithName("lastIdxId").description("마지막 원소 ID"),
+                                        parameterWithName("size").description("페이지 크기")
+                                ),
+                                pathParameters(
+                                        parameterWithName("userId").description("프로필 유저id")
+                                ),
+                                requestHeaders(
+                                        headerWithName("Authorization").description("accessToken")
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("결과코드"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("결과메시지"),
+                                        subsectionWithPath("data.content[]").type(JsonFieldType.ARRAY).description("리뷰 목록"),
+                                        fieldWithPath("data.content[].feedId").type(JsonFieldType.NUMBER).description("피드 ID"),
+                                        fieldWithPath("data.content[].userId").type(JsonFieldType.NUMBER).description("프로필 유저 ID"),
+                                        fieldWithPath("data.content[].storeId").type(JsonFieldType.NUMBER).description("식당 고유 Id"),
+                                        fieldWithPath("data.content[].storeName").type(JsonFieldType.STRING).description("식담명"),
+                                        fieldWithPath("data.content[].kakaoStoreId").type(JsonFieldType.NUMBER).description("식당 카카오 고유 ID"),
+                                        fieldWithPath("data.content[].feedImageUrl").type(JsonFieldType.STRING).description("피드 image URL"),
+                                        fieldWithPath("data.content[].feedCreatedAt").type(JsonFieldType.STRING).description("피드 작성 시기"),
+                                        fieldWithPath("data.content[].likeCnt").type(JsonFieldType.NUMBER).description("좋아요 수"),
+                                        fieldWithPath("data.content[].commentCnt").type(JsonFieldType.NUMBER).description("댓글 수"),
+                                        fieldWithPath("data.content[].isHeartFeed").type(JsonFieldType.BOOLEAN).description("좋아요 여부"),
+                                        subsectionWithPath("data.pageable").type(JsonFieldType.OBJECT).description("페이지 정보"),
+                                        fieldWithPath("data.pageable.pageNumber").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                                        fieldWithPath("data.pageable.pageSize").type(JsonFieldType.NUMBER).description("페이지 크기"),
+                                        subsectionWithPath("data.pageable.sort").type(JsonFieldType.OBJECT).description("정렬 정보"),
+                                        fieldWithPath("data.pageable.sort.empty").type(JsonFieldType.BOOLEAN).description("정렬 정보가 비어 있는지 여부"),
+                                        fieldWithPath("data.pageable.sort.sorted").type(JsonFieldType.BOOLEAN).description("정렬이 되어 있는지 여부"),
+                                        fieldWithPath("data.pageable.sort.unsorted").type(JsonFieldType.BOOLEAN).description("정렬이 되어 있지 않은지 여부"),
+                                        fieldWithPath("data.pageable.offset").type(JsonFieldType.NUMBER).description("페이지 오프셋"),
+                                        fieldWithPath("data.pageable.paged").type(JsonFieldType.BOOLEAN).description("페이지가 페이징 되어 있는지 여부"),
+                                        fieldWithPath("data.pageable.unpaged").type(JsonFieldType.BOOLEAN).description("페이지가 페이징 되어 있지 않은지 여부"),
+                                        fieldWithPath("data.first").type(JsonFieldType.BOOLEAN).description("첫 번째 페이지 여부"),
+                                        fieldWithPath("data.last").type(JsonFieldType.BOOLEAN).description("마지막 페이지 여부"),
+                                        fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("페이지 크기"),
+                                        fieldWithPath("data.number").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
+                                        subsectionWithPath("data.sort").type(JsonFieldType.OBJECT).description("정렬 정보"),
+                                        fieldWithPath("data.numberOfElements").type(JsonFieldType.NUMBER).description("현재 페이지의 요소 수"),
+                                        fieldWithPath("data.empty").type(JsonFieldType.BOOLEAN).description("현재 페이지가 비어 있는지 여부")
+                                )
                         )
-                    )
                 );
     }
 
     @Test
     void updateProfileNickname() throws Exception {
         //given
-        NicknameRequest nicknameRequest = new NicknameRequest("뉴닉네임");
+        ProfileNicknameRequest nicknameRequest = new ProfileNicknameRequest("뉴닉네임");
 
         doNothing().when(profileService).updateProfileNickname(any(), anyLong(), eq("뉴닉네임"));
 
